4eceee99f4688a71137ba810f741368f1d10f389
Duane Bester <duane@besterdesigns.com>
2013-10-26 18:07:08 -0500

    Bumped Dep version

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/zcaudate/sigmund"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.fusesource/sigar "1.6.4"]
                  [sigar/sigar-native-deps "1.6.4"]]
 
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
